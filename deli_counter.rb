1086d49d317c6fd40d6bc5d0a577d91e8ace6e0e
David Gilbert <dagilbert333@gmail.com>
2018-10-09 21:03:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,12 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else 
-   
+    message = "The people in line are: "
+    line_message = []
+    array.each_with_index do |person, index| 
+      line_message << " #{index+1}. #{person}"
+    end
+    return message + line_message.join(" ")
   end
 end
 
